f6520542c0e20c2b81e9e9350dd23967d4db6160
Theodore Osei Asimeng <theodoraldo@gmail.com>
2024-09-16 19:13:17 +0000

    Import created components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import HeroSection from "@/components/sections/HeroSection";
 import ScrollingTextSection from "@/components/sections/ScrollTextSection";
-
+import IconSection from "@/components/sections/IconSection";
+import Services from "@/components/sections/Services";
+import Company from "@/components/sections/Company";
 
 export default function Home() {
   return (
-   
-
-      <>
+    <>
       <HeroSection />
       <ScrollingTextSection />
-      </>
-     
+      <IconSection />
+      <Services />
+      <Company />
+    </>
   );
 }
