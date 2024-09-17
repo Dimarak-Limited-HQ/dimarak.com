ab71b3484a8981c16e0e26f176950207231db99d
“rich” <adukyerer@gmail.com>
2024-09-17 14:29:52 +0000

    Refactor ScrollTextSection and Footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 
-import BusinessMilestones from "@/components/sections/BusinessMilestones";
-import ClientTestimonials from "@/components/sections/ClientTestimonials";
-import HeroSection from "@/components/sections/HeroSection";
-import ScrollingTextSection from "@/components/sections/ScrollTextSection";
+
 import ServicesSection from "@/components/sections/ServicesSection";
 import WhyChooseUs from "@/components/sections/WhyChooseUs";
-import HeroSection from '@/components/sections/HeroSection';
 import ScrollingTextSection from '@/components/sections/ScrollTextSection';
-import { Footer } from '@/components/footer/footer';
+import Footer from "@/components/footer/Footer";
 import Testimonial from '@/components/testimonial/Testimonial';
-
 import HeroSection from "@/components/sections/HeroSection";
-import ScrollingTextSection from "@/components/sections/ScrollTextSection";
 import IconSection from "@/components/sections/IconSection";
-import Services from "@/components/sections/Services";
-import Company from "@/components/sections/Company";
-import About from "@/components/sections/About";
 
 
 
@@ -24,19 +15,10 @@ export default function Home() {
     <>
       <HeroSection />
       <ScrollingTextSection />
-
-      <BusinessMilestones/>
-      <ServicesSection/>
-      <WhyChooseUs/>
-      <ClientTestimonials/>
-      </>
-   
-      <Testimonial />
-      <Footer />
       <IconSection />
-      <Services />
-      <Company />
-      <About />
+      <ServicesSection />
+      <WhyChooseUs />
+      <Testimonial />
     </>
   );
 }
