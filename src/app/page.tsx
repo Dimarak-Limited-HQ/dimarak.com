36c2e2ebee2a921404398aac3a912294639ddcc2
kessie <kayp3477@gmail.com>
2024-09-20 08:38:56 +0000

    Add WhoWeAre section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-
-
-import ServicesSection from "@/components/sections/ServicesSection";
-import WhyChooseUs from "@/components/sections/WhyChooseUs";
+import ServicesSection from '@/components/sections/ServicesSection';
+import WhyChooseUs from '@/components/sections/WhyChooseUs';
+import WhoWeAre from '@/components/sections/WhoWeAre';
 import ScrollingTextSection from '@/components/sections/ScrollTextSection';
-import Footer from "@/components/footer/Footer";
 import Testimonial from '@/components/testimonial/Testimonial';
-import HeroSection from "@/components/sections/HeroSection";
-import IconSection from "@/components/sections/IconSection";
-
-
+import HeroSection from '@/components/sections/HeroSection';
+import IconSection from '@/components/sections/IconSection';
 
 export default function Home() {
   return (
@@ -17,6 +13,7 @@ export default function Home() {
       <ScrollingTextSection />
       <IconSection />
       <ServicesSection />
+      <WhoWeAre />
       <WhyChooseUs />
       <Testimonial />
     </>
