6daf7810c020f43675cc0cae74af4f7a285672fc
kessie <kayp3477@gmail.com>
2024-09-21 16:48:47 +0000

    Changed gradient color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,30 @@
-import type { Config } from "tailwindcss";
-const flowbite = require("flowbite-react/tailwind");
+import type { Config } from 'tailwindcss';
+const flowbite = require('flowbite-react/tailwind');
 
 const config: Config = {
-  darkMode: "class",
+  darkMode: 'class',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     flowbite.content(),
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#1A73E8",
-        body: "#212121",
-        voilet: "#0000EE",
-        link: "#172b4d",
-        dark: "#12192B",
-        accent: "#FFB74D",
-        paragraph: "#6B7280",
+        primary: '#1A73E8',
+        body: '#212121',
+        voilet: '#0000EE',
+        link: '#172b4d',
+        dark: '#12192B',
+        accent: '#FFB74D',
+        paragraph: '#6B7280',
       },
       backgroundImage: {
-        "hero-pattern": "url('/particle.png')",
-        "hero-image": "url('/Dimarhero.png')",
-        "gradient-three-color":
-          "linear-gradient(to right, #82CAFF 5%, #92C7C7 40%, #50C878 70%, #D4FF8F 100%)",
+        'hero-pattern': "url('/particle.png')",
+        'hero-image': "url('/Dimarhero.png')",
+        'gradient-three-color':
+          'linear-gradient(to right, #82CAFF 5%, #92C7C7 40%, #50C878 70%, #BEE580 100%)',
       },
     },
   },
